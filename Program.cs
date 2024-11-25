e0bd676bcc8533d2d05e0efbb821224448af30d3
Unai <unai.arevalo.7e8@itb.cat>
2024-11-25 12:22:38 +0100

    Add cs program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace intValues 
+{
+    public class intValues
+    {
+        public static void Main()
+        {
+            int quantiti;
+            try
+            {
+                Console.WriteLine("Introdueix la quantitat de valors que vols introduir");
+                quantiti = Convert.ToInt32(Console.ReadLine());
+                int[] quants = new int[quantiti];
+                for (int i = 0; i < quantiti; i++)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Introdueix un valor enter");
+                    quants[i] = Convert.ToInt32(Console.ReadLine());
+                }
+                for (int i = 0;i < quants.Length; i++)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"Valor número {i + 1}");
+                    Console.WriteLine(quants[i]);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Error en l'execucuó");
+            }
+        }
+    }
+}
